140433ba595a097021e2af8460bdf41e71fb0a3e
bo_olk@outlook.com <bo_olk@outlook.com>
2018-08-24 15:52:59 +0700

    first commit

---
+++ src/providers/rest/rest.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the RestProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class RestProvider {
+  apiUrl = 'http://localhost:80/ens/api';
+  constructor(public http: HttpClient) {
+    console.log('Hello RestProvider Provider');
+  }
+
+
+  getUsers() {
+    return new Promise(resolve => {
+      this.http.get(this.apiUrl+'/LISTOFVALUE/getAllMonth.php').subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+
+  addUser(data) {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.apiUrl+'/users', JSON.stringify(data))
+        .subscribe(res => {
+          resolve(res);
+        }, (err) => {
+          reject(err);
+        });
+    });
+  }
+
+
+
+
+
+
+}
